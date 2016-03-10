3f0815e90c63c77c3b0645308a86144b464b22c4
Shuai Zhang <nicolasloco@gmail.com>
2016-03-10 16:18:55 -0500

    initial commit

---
+++ src/main/java/edu/neu/cloudaddy/config/Application.java
@@ -0,0 +1,17 @@
+package edu.neu.cloudaddy.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "edu.neu.cloudaddy")
+@EnableAutoConfiguration
+public class Application {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
