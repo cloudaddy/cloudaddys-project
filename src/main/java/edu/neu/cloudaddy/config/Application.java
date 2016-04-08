4b98bef9aebbf3de1c20e3efae6a5948af72d326
Shuai Zhang <nicolasloco@gmail.com>
2016-04-08 15:38:09 -0400

    changing the spring boot application to enable a deployable war file

--- src/main/java/edu/neu/cloudaddy/config/Application.java
+++ src/main/java/edu/neu/cloudaddy/config/Application.java
@@ -3,15 +3,22 @@ package edu.neu.cloudaddy.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "edu.neu.cloudaddy")
 @EnableAutoConfiguration
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) throws Throwable {
-        SpringApplication.run(Application.class, args);
-    }
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(Application.class, args);
+	}
 
 }
