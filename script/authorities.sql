1be3ef71cb9f7f70e93171bd290e7b5c6d48fbc2
rachita <bansal.r@husky.neu.edu>
2016-04-10 22:52:41 -0400

    adding tmp

--- script/authorities.sql
+++
@@ -1,4 +0,0 @@
- create table `authorities` ( `username` varchar(50) not null, 
-			      `authority` varchar(50) not null);
-
-INSERT INTO `authorities` (`username`,`authority`) values ("user","USER");
