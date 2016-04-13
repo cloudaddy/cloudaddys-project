e8b96602d705b3e407c989a6e2e4c66cf55a2cfa
Shuai Zhang <nicolasloco@gmail.com>
2016-04-13 01:01:30 -0400

    correcting sql errors

--- script/users_password.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE `users_password` (
-  `id` mediumint(8) unsigned default NULL,
-  `password` varchar(255),
-  FOREIGN KEY (`id`) REFERENCES `users`(`id`)
-);
-
-
-INSERT INTO `users_password` (`id`,`password`) VALUES ("101","user");
