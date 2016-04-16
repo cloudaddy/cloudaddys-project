8bed1e14a71aaf72847cfe418eef09288e44c6ac
rachita <bansal.r@husky.neu.edu>
2016-04-16 17:57:43 -0400

    added junit test

---
+++ src/test/edu/neu/cloudaddy/test/MvcTestController.java
@@ -0,0 +1,34 @@
+package edu.neu.cloudaddy.test;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+
+import edu.neu.cloudaddy.controllers.HomeController;
+
+public class MvcTestController {
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() throws Exception {
+		this.mockMvc = standaloneSetup(new HomeController()).alwaysExpect(
+				status().isOk()).build();
+	}
+
+	@Test
+	public void simple() throws Exception {
+		standaloneSetup(new HomeController())
+				.build()
+				.perform(get("/"))
+				.andExpect(status().isOk())
+				.andExpect(
+						content().contentType("text/plain;charset=ISO-8859-1"))
+				.andExpect(content().string("home"));
+	}
+}
