dfa536eb01713c61a092b4bb537985e4b2dfd55a
Shuai Zhang <nicolasloco@gmail.com>
2016-04-16 22:22:58 -0400

    adding retry mechanism in validate_service

--- deploy/scripts/validate_service.sh
+++ deploy/scripts/validate_service.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-sleep 2m
-result=$(curl -s http://localhost:8080/)
+validate(){
+  result=$(curl -s http://localhost:8080/)
 
-if [[ "$result" =~ "Cloudaddy" ]]; then
-    echo "Cloudaddy is up and running!";
-    exit 0
-else
-    echo "Cloudaddy isn't up and running!";
-    exit 1
-fi
+  if [[ "$result" =~ "Cloudaddy" ]]; then
+      echo "Cloudaddy is up and running!";
+      return 0
+    else
+      echo "Cloudaddy isn't up and running!";
+      return 1
+  fi
+}
+
+n=0
+until [ $n -ge 5 ]
+do
+  validate && exit 0  # substitute your command here
+  n=$[$n+1]
+  sleep 30s
+  echo "Retrying..."
+done
+
+echo "Failed after $n times of retrying."
+
+exit 1
