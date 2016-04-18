449fbe0c2bbf2a4ffa42b999677788fdeedf3b85
LiuShang <liushangllx@126.com>
2016-04-18 15:25:10 -0400

    enabling SSL mode

--- deploy/UserData.sh
+++ deploy/UserData.sh
@@ -11,6 +11,48 @@ sudo ./install auto
 
 # install tomcat and java
 sudo apt-get -y install tomcat7
+
+# enable SSL model
+echo '<?xml version="1.0" encoding="utf-8"?>
+<Server port="8005" shutdown="SHUTDOWN">
+<Listener className="org.apache.catalina.core.JasperListener" />
+<Listener className="org.apache.catalina.core.JreMemoryLeakPreventionListener" />
+<Listener className="org.apache.catalina.mbeans.GlobalResourcesLifecycleListener" />
+<Listener className="org.apache.catalina.core.ThreadLocalLeakPreventionListener" />
+<GlobalNamingResources>
+<Resource name="UserDatabase" auth="Container"
+type="org.apache.catalina.UserDatabase"
+description="User database that can be updated and saved"
+factory="org.apache.catalina.users.MemoryUserDatabaseFactory"
+pathname="conf/tomcat-users.xml" />
+</GlobalNamingResources>
+<Service name="Catalina">
+<Connector
+port="8080"
+protocol="HTTP/1.1"
+proxyPort="443"
+scheme="https"
+secure="true"
+proxyName="www.cloudaddy.us"
+connectionTimeout="20000"
+URIEncoding="UTF-8"
+redirectPort="8443" />
+<Engine name="Catalina" defaultHost="localhost">
+<Realm className="org.apache.catalina.realm.LockOutRealm">
+<Realm className="org.apache.catalina.realm.UserDatabaseRealm"
+resourceName="UserDatabase"/>
+</Realm>
+<Host name="localhost"  appBase="webapps"
+unpackWARs="true" autoDeploy="true">
+<Valve className="org.apache.catalina.valves.AccessLogValve" directory="logs"
+prefix="localhost_access_log." suffix=".txt"
+pattern="%h %l %u %t &quot;%r&quot; %s %b" />
+</Host>
+</Engine>
+</Service>
+</Server>
+' | sudo tee /var/lib/tomcat7/conf/server.xml
+
 # delete the default page
 sudo rm -rf /var/lib/tomcat7/webapps/ROOT
 exit 0
